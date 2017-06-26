f44da0b223f91639f3379c713e0b4bf103824df1
Liddleriddle <sebastian.rittler@web.de>
2017-06-26 13:32:19 +0200

    Files modularized
    - code/body.html
    - code/head.html
    - pages/komponenten.php
    - pages/lieferanten.php
    - pages/raeume.php

---
+++ pages/komponenten.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+		<title>MAUT - R&auml;ume</title>
+		
+		<?php include '\code\head.html'; ?>
+	</head>
+	
+<body>
+
+<!-- Sidebar -->
+<?php include '\code\body.html'; ?>
+
+<!-- Page Content -->
+<div style="margin-left:15%">
+
+<div class="w3-container w3-teal">
+  <h1>R&auml;ume</h1>
+</div>
+
+<div class="w3-container">
+ <table class="table">
+	<thead>
+	  <tr>
+		<th>Firstname</th>
+		<th>Lastname</th>
+		<th>Email</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  <tr>
+		<td>John</td>
+		<td>Doe</td>
+		<td>john@example.com</td>
+	  </tr>
+	  <tr>
+		<td>Mary</td>
+		<td>Moe</td>
+		<td>mary@example.com</td>
+	  </tr>
+	  <tr>
+		<td>July</td>
+		<td>Dooley</td>
+		<td>july@example.com</td>
+	  </tr>
+	</tbody>
+  </table>
+</div>
+
+</div>
+      
+</body>
+</html>
