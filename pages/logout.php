bceaecc774b914fc602d8893d95abf83d078301a
CaptainSporkey <CaptainSporkey@users.noreply.github.com>
2017-06-27 10:07:26 +0200

    created login/logout
    - pages\login.php
    - pages\logout.php

---
+++ pages/logout.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    session_destroy();
+?>
+
+<!doctype HTML>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0; URL=../pages/login.php" />
+    </head>
+</html>
