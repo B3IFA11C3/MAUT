9e6d5625ef262cfcfc713bd3f0bf66c187bc4d97
Neovar <tsutey93@freenet.de>
2017-06-27 13:45:34 +0200

    2. Datenbank hinzugefügt, minor changes 3

--- code/config.inc.php
+++ code/config.inc.php
@@ -3,6 +3,7 @@
         "SQLConn" => "127.0.0.1",
         "SQLUser" => "root",
         "SQLPass" => "",
-        "SQLDaBa" => "maut",
+        "SQLUserDB" => "user",
+        "SQLMautDB" => "maut"
     );
 ?>
--- code/utils.php
+++ code/utils.php
@@ -3,8 +3,8 @@
     function checklogin ($user, $passw) {
         if (!isset($user) || !isset($passw))
             return false;
-        $result = sqldoitarr("SELECT U_Passwort FROM User WHERE U_Benutzername = '".sqlmask($user)."'");
-        if ($result && $result[0][0] !== crypt($passw))
+        $result = sqldoitarr($CONFIG["SQLUserDB"], "SELECT U_Passwort FROM User WHERE U_Benutzername = '".sqlmask($user)."'");
+        if ($result && $result[0][0] !== crypt($passw, $result[0][0]))
             return false;
         return true;
     }
@@ -14,10 +14,10 @@
             return "Bitte altes Passwort, neues Passwort und neues Passwort wiederholen füllen!";
         if ($newpw !== $newpwwdh)
             return "Das neue Passwort stimmt nicht mit der Wiederholung überein!";
-        $result = sqldoitarr("SELECT U_Passwort FROM User WHERE U_Benutzername = '".sqlmask($user)."'");
-        if ($result && $result[0][0] !== crypt($oldpw))
+        $result = sqldoitarr($CONFIG["SQLUserDB"], "SELECT U_Passwort FROM User WHERE U_Benutzername = '".sqlmask($user)."'");
+        if ($result && $result[0][0] !== crypt($oldpw, $result[0][0]))
             return "Das eingegebene Passwort ist nicht korrekt!";
-        if (sqldoit("UPDATE User SET U_Passwort = '".crypt($newpw)."' WHERE U_Benutzername = '".sqlmask($user)."'"))
+        if (sqldoit($CONFIG["SQLUserDB"], "UPDATE User SET U_Passwort = '".crypt($newpw)."' WHERE U_Benutzername = '".sqlmask($user)."'"))
             return "Passwort geändert!";
         return "Interner Fehler bei der Datenbankkommunikation";
     }
@@ -26,17 +26,17 @@
         if (count($sqlfields) != count($sqlvalues))
             return "Interner Fehler (".count($sqlvalues)." Values für ".count($sqlfields)." Felder)";
         $sqlstr = "INSERT INTO ".$sqltable."('".join("', '", array_map("sqlmask", $sqlfields))."') VALUES ('".join("', '", array_map("sqlmask", $sqlvalues))."')";
-        if (sqldoit($sqlstr))
+        if (sqldoit($CONFIG["SQLMautDB"], $sqlstr))
             return "Eintrag eingefügt!";
         return "Interner Fehler bei der Datenbankkommunikation";
     }
 
-    function sqldoit ($sqlstr) {
+    function sqldoit ($sqldb, $sqlstr) {
         if (!isset($sqlstr) || !$sqlstr)
             return false;
         
         $connect = mysqli_connect($CONFIG["SQLConn"], $CONFIG["SQLUser"], $CONFIG["SQLPass"]);
-        mysqli_select_db($CONFIG["SQLDaBa"]);
+        mysqli_select_db($sqldb);
 
         $result = mysqli_query($connect, $sqlstr);
         mysqli_close($connect);
@@ -44,8 +44,8 @@
         return $result;
     }
 
-    function sqldoitarr ($sqlstr) {
-        $result = sqldoit($sqlstr);
+    function sqldoitarr ($sqldb, $sqlstr) {
+        $result = sqldoit($sqldb, $sqlstr);
         if (!$result)
             return false;
         if (is_bool($result))
