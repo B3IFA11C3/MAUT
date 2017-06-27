3847312a23222f92fa11a8bb69f775942bf13d01
Fabian Vogt <fabian@ritter-vogt.de>
2017-06-27 12:56:33 +0200

    Add module and page skeleton

---
+++ js/maut.js
@@ -0,0 +1,121 @@
+   function filterTable(table, filter)
+    {
+		var words = filter.toLowerCase().split(" ");
+
+        for(var i = 0; i < table.tBodies.length; ++i)
+        {
+				var tbody = table.tBodies[i];
+				var row = tbody.rows[0];
+
+                var found = false;
+                if(words.length == 1 && words[0].length == 0)
+					found = true;
+				else
+				{
+					for(var w = 0; w < words.length && !found; ++w)
+						for(var c = 0; words[w].length > 0 && c < row.cells.length && !found; ++c)
+						{
+							var text = row.cells[c].innerText.toLowerCase();
+							if(text.indexOf(words[w]) !== -1)
+								found = true;
+						}
+				}
+
+				tbody.style.display = found ? "" : "none";
+        }
+    }
+
+	function makeAccordions()
+	{
+		var clickables = $(".tbodytable * tr.clickable");
+		for(var i = 0; i < clickables.length; ++i)
+		{
+			clickables[i].onclick = function() {
+				if(this.parentNode.classList.contains("accordion"))
+				{
+					// Hide other accordions
+					$(".tbodytable * tr.clickable").each(function(i,e) {
+						e.parentNode.classList.add("accordion");
+					});
+
+					this.parentNode.classList.remove("accordion");
+				}
+				else
+					this.parentNode.classList.add("accordion");
+			};
+		}
+	}
+
+	makeAccordions();
+
+	function makeSorttable(tableID)
+	{
+		var table = $(document.getElementById(tableID)),
+			tbody = table.find('tbody'),
+			th_index = 0,
+			th_sortType = "string";
+
+		table.find('th').data("sort-direction","ASC");
+
+		function getSortList(i, elem)
+		{
+			var txt = $("td", elem).eq(th_index).text();
+			$(elem).attr("data-sortval", txt);
+		}
+
+		function sortAsc(a, b)
+		{
+			var aData = $(a).attr("data-sortval"),
+				bData = $(b).attr("data-sortval");
+
+			if(th_sortType==="int")
+				return +bData < +aData ? 1 : -1; // Integer
+			else
+				return  bData <  aData ? 1 : -1; // String or else
+		}
+
+		function sortDesc(a, b)
+		{
+			var aData = $(a).attr("data-sortval"),
+				bData = $(b).attr("data-sortval");
+
+			if(th_sortType==="int")
+				return +bData > +aData ? 1 : -1; // Integer
+			else
+				return  bData >  aData ? 1 : -1; // String or else
+		}
+
+		//header sort
+		$("th", table).on("click", function() {
+			//toggle the sorting direction
+			$(this).data('sort-direction', ( $(this).data('sort-direction') == 'ASC' ? 'DESC' : 'ASC'));
+			var dir = $(this).data('sort-direction');
+
+			th_sortType = $(this).data('sort'); //get "int" or "string" from data-sort on th
+			th_index = $(this).index();
+
+			tbody = table.find('tbody').each(getSortList);
+
+			table.find('.sort-indicator').removeClass('sort-desc').removeClass('sort-asc');
+
+			if ( dir == "ASC") {
+				$(this).find('.sort-indicator').addClass('sort-asc'); //css class for ASC arrow
+				tbody.sort(sortAsc).detach().appendTo(table); //sort table
+			} else {
+				$(this).find('.sort-indicator').addClass('sort-desc'); //css class for DESC arrow
+				tbody.sort(sortDesc).detach().appendTo(table);  //sort table
+			}
+		});
+	}
+
+			function myAccFunc() {
+				var x = document.getElementById("demoAcc");
+				if (x.className.indexOf("w3-show") == -1) {
+					x.className += " w3-show";
+					x.previousElementSibling.className += " w3-green";
+				} else { 
+					x.className = x.className.replace(" w3-show", "");
+					x.previousElementSibling.className = 
+					x.previousElementSibling.className.replace(" w3-green", "");
+				}
+			}
---
+++ modules/01login/module.php
@@ -0,0 +1,86 @@
+<?php
+
+function login_get_data()
+{
+	return isset($_SESSION["user"]) ? $_SESSION["user"] : false;
+}
+
+function render_main_block($state="", $redirect_path="")
+{
+	$content = '<section id="login">
+            <div class="container" style="width: 400px">
+                <div class="row">
+                        <div class="form-wrap">
+                        <h1><img src="../img/logo.png" width="150px" /></h1>
+                            <form role="form" method="post" action="/login" id="login-form" autocomplete="off">
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" class="form-control" placeholder="Benutzername">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="key" class="form-control" placeholder="Passwort">
+                                </div>';
+
+	if($redirect_path != "")
+		$content .= '<input type="hidden" name="path" value="' . htmlentities($path) . '"/>';
+
+	$content .= '<input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Anmelden">    
+                            </form>';
+	if($state == "success")
+		$content .= '<div class="alert alert-success" role="alert"><center>Erfolgreich abgemeldet.</center></div>';
+	else if($state == "error")
+		$content .= '<div class="alert alert-danger" role="alert"><center><b>Fehler!</b> Anmeldedaten sind nicht korrekt.</center></div>';
+		
+	$content .= '</div>
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </section>';
+
+	return $content;
+}
+
+function login_show_login($path)
+{
+	if(login_get_data() !== false)
+		return false;
+
+	page_render(render_main_block("", $path), false);
+	return true;
+}
+
+function login_login($path)
+{
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	
+	$_SESSION["user"] = array("asdf");
+
+	if(login_get_data() !== false)
+	{
+		render_path($_POST["path"]);
+	}
+	else
+		page_render(render_main_block("error", $_POST["path"]), false);
+
+	return true;
+}
+
+function login_logout($path)
+{
+	if(login_get_data() !== false)
+	{
+		unset($_SESSION["user"]);
+		$state = "success";
+	}
+	else
+		$state = "error";
+
+	page_render(render_main_block($state), false);
+	return true;
+}
+
+maut_register_path("#^login\$#", login_login);
+maut_register_path("#.*#", login_show_login);
+maut_register_path("#^logout\$#", login_logout);
+
+return true;
+?>
