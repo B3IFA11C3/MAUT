c31c341009cfb14874ba25e060c8442edfe5ba06
Neovar <tsutey93@freenet.de>
2017-06-27 09:58:50 +0200

    minor changes

--- code/utils.php
+++ code/utils.php
@@ -2,8 +2,8 @@
     function sqlinsert ($sqlconn, $sqluser, $sqlpass, $sqldaba, $sqltable, $sqlfields, $sqlvalues) {
         if (count($sqlfields) != count($sqlvalues))
             return "Interner Fehler (".count($sqlvalues)." Values für ".count($sqlfields)." Felder)";
-        $sqlstr = "INSERT INTO ".$sqltable."(".join(", ", $sqlfields).") VALUES (".join(", ", $sqlvalues).")";
-        if (sqldoit($sqlconn, $sqluser, $sqlpass, $sqldaba, sqlmask($sqlstr)))
+        $sqlstr = "INSERT INTO ".$sqltable."('".join("', '", array_map("sqlmask", $sqlfields))."') VALUES ('".join("', '", array_map("sqlmask", $sqlvalues))."')";
+        if (sqldoit($sqlconn, $sqluser, $sqlpass, $sqldaba, $sqlstr))
             return "Eintrag eingefügt!";
         return "Interner Fehler bei der Datenbankkommunikation";
     }
@@ -22,7 +22,8 @@
     }
 
     function sqlmask ($sqlstring) {
-        return str_replace("'", "\\'", $sqlstring);
+        return mysqli_real_escape_string($sqlstring);
+        //return str_replace("'", "\\'", $sqlstring);
     }
 
     function changepw ($sqlconn, $sqluser, $sqlpass, $sqldaba, $user, $oldpw, $newpw, $newpwwdh) {
@@ -30,10 +31,10 @@
             return "Bitte altes Passwort, neues Passwort und neues Passwort wiederholen füllen!";
         if ($newpw !== $newpwwdh)
             return "Das neue Passwort stimmt nicht mit der Wiederholung überein!";
-        $result = sqldoit($sqlconn, $sqluser, $sqlpass, $sqldaba, "SELECT password FROM benutzer WHERE user = '".$user."'");
+        $result = sqldoit($sqlconn, $sqluser, $sqlpass, $sqldaba, "SELECT password FROM benutzer WHERE user = '".sqlmask($user)."'");
         if (mysqli_fetch_assoc($result)[0] !== $oldpw)
             return "Das eingegebene Passwort ist nicht korrekt!";
-        if (sqldoit($sqlconn, $sqluser, $sqlpass, $sqldaba, "UPDATE benutzer SET password = ".sqlmask($newpw)." WHERE user = '".$user."'"))
+        if (sqldoit($sqlconn, $sqluser, $sqlpass, $sqldaba, "UPDATE benutzer SET password = '".sqlmask($newpw)."' WHERE user = '".sqlmask($user)."'"))
             return "Passwort geändert!";
         return "Interner Fehler bei der Datenbankkommunikation";
     }
