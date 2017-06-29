4b299f8a95407fefaa95466c3e10cedfaaa7901c
Pascal Tänzer <p.taenzer@gmx.net>
2017-06-30 01:51:13 +0200

    WTF am i doing with my life? I might not get much sleep, but at least the Komponenten arten are now working to my satisfacton. Worst thing is: apart from the fact that no one will ever see this site after we got our grades, this would be the least used part in the whole software.

--- modules/komponentenarten/module.php
+++ modules/komponentenarten/module.php
@@ -3,48 +3,47 @@
 require_once("code/table.php");
 require_once("code/tablefunctions.php");
 
+//wird beschrieben durch
+//gibt zurück, ob die Komponentenart das Attribut enthält
+function wbs($ka, $kat) {
+	foreach($ka['ka_spalten'] as $ka_kat) {
+		if($ka_kat['kat_id'] == $kat['kat_id']) return true;
+	}
+	return false;
+}
+
 function komponentenarten_render_row($komponentenart)
 {
-	$content = '<div class="divslider">
-					<div> <!-- Left side of Divslider -->
-						<div>
-						<form method="POST" style="display: table; width: 100%;">
-							<input type="hidden" name="ka[ka_id]" value="' . htmlentities($komponentenart["ka_id"]) . '"/>
-							<table class="table">
-								<tr>
-									<td>Einmalig:&nbsp;</td> 
-									<td>' . ($komponentenart["ka_einmalig"] ? "Ja" : "Nein") . '</td>
-								</tr>
-								<tr>
-									<td>Attribute:&nbsp;</td> <td>';
-										foreach($komponentenart['ka_spalten'] as $attr) {
-											$content .= $attr['kat_bezeichnung'] . ' (' . $attr['kat_typ'] . ') <br>'; 
+	$content = '<div style="margin-left: 15px;">
+					<form method="POST" style="display: table; width: 100%;">
+						<input type="hidden" name="ka[ka_id]" value="' . htmlentities($komponentenart["ka_id"]) . '"/>
+						<table class="editable_edit_table"><tbody>
+							<tr>
+								<td>Name:&nbsp;</td> 
+								<td><input class="edit_' . $komponentenart['ka_id'] . '" type="text" name="ka[ka_komponentenart]" disabled="true" value="' . htmlentities($komponentenart["ka_komponentenart"]) . '"/></td>
+							</tr>
+							<tr>
+								<td>Attribute:&nbsp;</td> 
+								<td> 
+									<select name="ka[attribute][]" disabled="true" class="chosen-select edit_' . $komponentenart['ka_id'] . '" id="edit_attributes_' . $komponentenart['ka_id'] . '" multiple data-placeholder="keine Attribute vorhanden">';
+										foreach(Componentattributes::list_all() as $attr) {
+											$content .= '<option value="' . $attr['kat_id'] . '" ' . (wbs($komponentenart, $attr)?'selected':'') . ' >' . htmlentities($attr['kat_bezeichnung']) . ' (' . htmlentities($attr['kat_typ']) . ')</option>';
 										}
 										$content .= '
-									</td>
-								</tr>
-							</table>
-							<div class="switch" style="position: initial;">
-								<button type="submit" class="btn btn-primary" name="action" value="delete">L&ouml;schen</button>
-								<button type="button" class="btn btn-primary" onClick="divSliderShowRight(this.closest(\'.divslider\'))">Bearbeiten</button>
-							</div>
-						</form>
-						</div>
-					</div>
-
-					<div> <!-- Right side of Divslider -->
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>Einmalig:&nbsp;</td> 
+								<td><input name="ka[ka_einmalig]" type="checkbox" class="edit_' . $komponentenart['ka_id'] . '" disabled="true" ' . ($komponentenart["ka_einmalig"]?"checked":"") . '/></td>
+							</tr>
+						</tbody></table>
 						<div>
-						<form method="POST" style="display: table; width: 100%;">
-							<input type="hidden" name="ka[ka_id]" value="' . htmlentities($komponentenart["ka_id"]) . '"/>
-							<label style="display: table-cell">Einmalig: <input type="checkbox" ' . ($komponentenart["ka_einmalig"] ? "checked" : "") . ' name="ka[ka_einmalig]"/></label>
-							<label style="display: table-cell">Name: <input type="text" name="ka[ka_komponentenart]" value="' . htmlentities($komponentenart["ka_komponentenart"]) . '"/></label>
-							<div class="switch" style="position: initial;">
-								<button class="btn btn-primary" type="submit" name="action" value="save">Speichern</button>
-								<button class="btn btn-primary" type="button" onClick="divSliderShowLeft(this.closest(\'.divslider\'))">Zur&uuml;ck</button>
-							</div>
-						</form>
+							<button type="submit" class="btn btn-primary" name="action" value="delete">L&ouml;schen</button>
+							<button type="button" class="btn btn-primary" onclick="toggleEditing(' . $komponentenart['ka_id'] . ');">Bearbeiten</button>
+							<button type="submit" class="btn btn-primary edit_' . $komponentenart['ka_id'] . '" disabled="true" name="action" value="save">Speichern</button>
 						</div>
-					</div>
+					</form>
 				</div>';
 
 	return array("cols" => array($komponentenart["ka_id"], $komponentenart["ka_komponentenart"]),
@@ -66,6 +65,10 @@ function komponentenarten_show()
 		{
 			$value = array("ka_komponentenart" => $_POST["ka"]["ka_komponentenart"],
 						   "ka_einmalig" => (isset($_POST["ka"]["ka_einmalig"]) ? 1 : 0));
+            sqldelete("wird_beschrieben_durch", [["ka_id", (int)$_POST["ka"]["ka_id"]]]);			
+			foreach($_POST["ka"]["attribute"] as $attr) { 
+				Componenttypes::addattr((int)$_POST["ka"]["ka_id"], $attr);
+			}
 			$success = Componenttypes::change((int)$_POST["ka"]["ka_id"], $value);
 		}
 		else if($_POST["action"] == "insert")
@@ -75,7 +78,6 @@ function komponentenarten_show()
                             "ka_einmalig" => (isset($_POST["ka"]["ka_einmalig"]) ? 1 : 0));
 
                 $added = Componenttypes::add($value);
-                var_dump($_POST["ka"]["attribute"]);
                 foreach($_POST["ka"]["attribute"] as $attr) { 
                     Componenttypes::addattr($added, $attr);
                 }
@@ -92,8 +94,8 @@ function komponentenarten_show()
 	}
 	
 	$insert = '<div><div style="padding: 10.5px">
-					<form method="POST" style="display: table; width: 100%;">
-						<table>
+					<form method="POST" style="display: table; width: 100%; padding: 15px;">
+						<table class="editable_edit_table">
 							<tr>
 								<td>Name:&nbsp;</td> <td><input type="text" name="ka[ka_komponentenart]"/></td>
 							</tr>
@@ -113,7 +115,7 @@ function komponentenarten_show()
 								<td><input type="checkbox" checked name="ka[ka_einmalig]"/></td>
 							</tr>
 						</table>
-						<div class="switch" style="position: initial;">
+						<div>
 							<button type="submit" class="btn btn-primary" name="action" value="insert">Speichern</button>
 						</div>
 					</form>
@@ -122,7 +124,50 @@ function komponentenarten_show()
 	$content .= table_render(array("ID" => "int", "Name" => "string"),
 			array_map("komponentenarten_render_row", Componenttypes::list_all()),
 			array("header" => "Komponentenart hinzuf&uuml;gen", "content" => $insert));
-    $content .= '<script type="text/javascript">$(".chosen-select").chosen();</script>';
+    $content .= '<script type="text/javascript">
+					$(".chosen-select").chosen();
+					function toggleEditing(id) {
+						$(".edit_"+id).each(
+							function(i,e){
+								if(e.disabled==true){
+									e.disabled=false; 
+									$("#edit_attributes_"+id).trigger("chosen:updated");
+								}
+								else{
+									e.disabled=true;
+									$("#edit_attributes_"+id).trigger("chosen:updated");
+								} 
+							} );
+					}
+				</script>
+				<style>
+					.editable_edit_table {
+						border: none;
+					}
+
+					.editable_edit_table > tbody > tr {
+						border: none;
+						border-top: 1px solid grey;
+					}
+
+					.editable_edit_table > tbody > tr:first-child {
+						border-top: none;
+					}
+
+					.editable_edit_table > tbody > tr > td {
+						border: none;
+						border-left: 1px solid grey;
+						padding: 5px;
+					}
+
+					.editable_edit_table > tbody > tr > td:first-child {
+						border-left: none;
+						border-right: 2px solid grey;	
+						text-align: right;					
+					}				
+
+					
+				</style>';
 
 	page_render($content);
 	return true;
