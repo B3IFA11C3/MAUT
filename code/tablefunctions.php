acc9dcdec032286c5ae070037f40d2f15f64ba23
Liddleriddle <sebastian.rittler@web.de>
2017-06-29 12:34:18 +0200

    bugfix
    Select war falsch

--- code/tablefunctions.php
+++ code/tablefunctions.php
@@ -42,7 +42,7 @@ class Components {
         $result = sqlselect("komponenten", $cols);
 		
 		$result = addtoarray($result,'lieferant','select * from lieferanten where l_id = ?','l_id');
-		$result = addtoarray($result,'komponentenattribute','select ka.* , h.* from wird_beschrieben_durch b, komponentenattribute ka (left join komponenten_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
+		$result = addtoarray($result,'komponentenattribute','select ka.* , h.* from wird_beschrieben_durch b, komponentenattribute ka (left join komponente_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
 		$result = addtoarray($result,'raeume','select * from raeume r , komponente_in_raum i where r.r_id = i.r_id and i.k_id = ?','k_id');
         return $result;
     }
@@ -69,7 +69,7 @@ class Rooms {
 		$result = addtoarray($result,'komponenten','select k.*, a.* from komponentenarten a , komponenten k , komponente_in_raum i where i.r_id = ? and i.k_id = k.k_id and k.ka_id = a.ka_id','r_id');
 		foreach ($result as &$row){
 			$row = addtoarray($row,'lieferant','select * from lieferanten where l_id = ?','l_id');
-			$row = addtoarray($row,'komponentenattribute','select * from wird_beschrieben_durch b, komponentenattribute ka (left join komponenten_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
+			$row = addtoarray($row,'komponentenattribute','select * from wird_beschrieben_durch b, komponentenattribute ka (left join komponente_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
 		}
         return $result;
     }
