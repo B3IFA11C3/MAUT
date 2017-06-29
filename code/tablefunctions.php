8e27e94009aac9a0e2fda170276d456101a60699
Liddleriddle <sebastian.rittler@web.de>
2017-06-29 14:43:18 +0200

    bug fix
    KP

--- code/tablefunctions.php
+++ code/tablefunctions.php
@@ -43,7 +43,7 @@ class Components {
         $result = sqlselect("komponenten", $cols);
 		
 		$result = addtoarray($result,'lieferant','select * from lieferanten where l_id = ?','l_id');
-		$result = addtoarray($result,'komponentenattribute','select ka.* , h.* from wird_beschrieben_durch as b, komponentenattribute as ka (left join komponente_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
+		$result = addtoarray($result,'komponentenattribute','select ka.* , h.* from wird_beschrieben_durch as b, komponentenattribute as ka left join komponente_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ? where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
 		$result = addtoarray($result,'raeume','select * from raeume as r , komponente_in_raum as i where r.r_id = i.r_id and i.k_id = ?','k_id');
         return $result;
     }
