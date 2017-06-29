4a4e7b8139da9111656fdac8b33acaa1e02b6abf
Neovar <tsutey93@freenet.de>
2017-06-29 11:01:16 +0200

    weitere tabelfunctions

--- code/tablefunctions.php
+++ code/tablefunctions.php
@@ -8,11 +8,11 @@ class Componentattributes {
     public static function add($vals) {
         return sqlassoinsert("komponentenattribute", $vals);
     }
-    public static function change($id, $vals) {
-        return sqlassoupdate("komponentenattribute", $vals, [["kat_id", $id]]);
+    public static function change($kat_id, $vals) {
+        return sqlassoupdate("komponentenattribute", $vals, [["kat_id", $kat_id]]);
     }
-    public static function delete($id) {
-        return sqldelete("komponentenattribute", [["kat_id", $id]]);
+    public static function delete($kat_id) {
+        return sqldelete("komponentenattribute", [["kat_id", $kat_id]]);
     }
 }
 class Componenttypes {
@@ -42,11 +42,11 @@ class Components {
     public static function add($vals) {
         return sqlassoinsert("komponenten", $vals);
     }
-    public static function change($id, $vals) {
-        return sqlassoupdate("komponenten", $vals, [["kat_id", $id]]);
+    public static function change($k_id, $vals) {
+        return sqlassoupdate("komponenten", $vals, [["k_id", $k_id]]);
     }
-    public static function delete($id) {
-        return sqldelete("komponenten", [["kat_id", $id]]);
+    public static function delete($k_id) {
+        return sqldelete("komponenten", [["k_id", $k_id]]);
     }
     public static function changeattr($k_id, $kat_id, $khkat_wert) {
         return sqlupdate("komponente_hat_attribute", [["khkat_wert", $khkat_wert]], [["k_id", $k_id], ["kat_id", $kat_id]]);
@@ -62,29 +62,29 @@ class Rooms {
     public static function add($vals) {
         return sqlassoinsert("raeume", $vals);
     }
-    public static function change($id, $vals) {
-        return sqlassoupdate("raeume", $vals, [["kat_id", $id]]);
+    public static function change($r_id, $vals) {
+        return sqlassoupdate("raeume", $vals, [["r_id", $r_id]]);
     }
-    public static function delete($id) {
-        return sqldelete("raeume", [["kat_id", $id]]);
+    public static function delete($r_id) {
+        return sqldelete("raeume", [["r_id", $r_id]]);
     }
-    /*public static function compinroom($id) {
-        return sqldelete("komponenten", [["kat_id", $id]]);
-    }*/
+    public static function addcompinroom($k_id, $r_id) {
+        return sqlinsert("komponente_in_raum", ["k_id", "r_id"], [$k_id, $r_id] );
+    }
 }
 class Supplier {
-    /*public static function list($cols = ["*"]) {
-        return sqlselect("komponentenattribute", $cols);
+    public static function list($cols = ["*"]) {
+        return sqlselect("lieferant", $cols);
     }
     public static function add($vals) {
-        return sqlassoinsert("komponentenattribute", $vals);
+        return sqlassoinsert("lieferant", $vals);
     }
-    public static function change($id, $vals) {
-        return sqlassoupdate("komponentenattribute", $vals, [["kat_id", $id]]);
+    public static function change($l_id, $vals) {
+        return sqlassoupdate("lieferant", $vals, [["l_id", $l_id]]);
     }
-    public static function delete($id) {
-        return sqldelete("komponentenattribute", [["kat_id", $id]]);
-    }*/
+    public static function delete($l_id) {
+        return sqldelete("lieferant", [["l_id", $l_id]]);
+    }
 }
 
 
