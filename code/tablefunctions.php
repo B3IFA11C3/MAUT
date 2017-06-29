9d29ec11adb051469601d2de0cf13a346a357655
Liddleriddle <sebastian.rittler@web.de>
2017-06-29 11:34:55 +0200

    components fetch
    implementiert

--- code/tablefunctions.php
+++ code/tablefunctions.php
@@ -16,9 +16,11 @@ class Componentattributes {
     }
 }
 class Componenttypes {
-    /*public static function list($cols = ["*"]) {
-        return sqlselect("komponentenarten", $cols);
-    }*/
+    public static function list($cols = ["*"]) {
+         $result = sqlselect("komponentenarten", $cols);
+		 $result = addtoarray($result,'ka_spalten','select ka.* from wird_beschrieben_durch b, komponentenattribute ka where b.ka_id = ? and b.kat_id = ka.kat_id ','ka_id');
+		 return $result;
+	}
     public static function add($vals) {
         return sqlassoinsert("komponentenarten", $vals);
     }
@@ -39,9 +41,8 @@ class Components {
     public static function list($cols = ["*"]) {
         $result = sqlselect("komponenten", $cols);
 		
-		
 		$result = addtoarray($result,'liefeant','select * from lieferanten where l_id = ?','l_id');
-		$result = addtoarray($result,'komponentenattribute','select * from wird_beschrieben_durch b, komponentenattribute ka (left join komponenten_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
+		$result = addtoarray($result,'komponentenattribute','select ka.* , h.* from wird_beschrieben_durch b, komponentenattribute ka (left join komponenten_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
 		$result = addtoarray($result,'raeume','select * from raeume r , komponente_in_raum i where r.r_id = i.r_id and i.k_id = ?','k_id');
         return $result;
     }
