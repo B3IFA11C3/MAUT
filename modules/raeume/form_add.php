d69cd8fa81b40abe9bf3e7e7b3105c76ea54ed70
Liddleriddle <sebastian.rittler@web.de>
2017-06-29 22:16:13 +0200

    Form_add Räume

--- modules/raeume/form_add.php
+++ modules/raeume/form_add.php
@@ -1,31 +1,24 @@
 <?php
-    function add_raum_show() {
-    $komponenten = array(
-        0 => array(
-            "name" => "Test1",
-            "art" => "Storage"
-        ),
-        1 => array(
-            "name" => "Test2",
-            "art" => "Server"
-        ),
-        2 => array(
-            "name" => "Test3",
-            "art" => "Switch"
-        ),
-        3 => array(
-            "name" => "Test4",
-            "art" => "Client-PC"
-        )
-    );
-    $komponenten_arten = array(
-        "Storage",
-        "Server",
-        "Switch",
-        "Client-PC",
-        "Client-PC"
-    );
-
+    function add_raum_show($components, $components_art) {
+    
+		$data_index = $rooms["r_id"];
+        $roomNr = $rooms["r_nr"];
+        $roomBezeichnung = $rooms["r_bezeichnung"];
+        $roomNotiz = $rooms["r_notiz"];  
+        
+        $currentComponents_Of_Room = $rooms["komponenten"];
+        
+        
+        $asdasd = array();
+        
+        foreach($components as $elementRight){
+            
+            foreach($currentComponents_Of_Room as $elementeLeft){
+                if($elementRight["k_id"] != $elementeLeft["k_id"]){
+                    $asdasd[] = $elementRight;
+                }
+            }         
+        }
 
 $str1= "<div class='row'>
         <div class='col-md-12'>
@@ -35,8 +28,8 @@ $str1= "<div class='row'>
                     <div class='col-md-1'></div>
                     
                     <div class='col-md-4'>
-                        <input type='text' placeholder='Raum-Nr.' name='roomnr' />
-                        <input type='text' placeholder='Bezeichnung' name='roomdsg' />
+                        <input type='text' class='changeEditStatus' placeholder='Raum-Nr.' disabled name='roomnr' value='$roomNr' />
+                        <input type='text' class='changeEditStatus' placeholder='Bezeichnung' value='$roomBezeichnung' disabled name='roomdsg' />
                     </div>
                     
                     <div class='col-md-1'>
@@ -45,12 +38,13 @@ $str1= "<div class='row'>
                     <div class='col-md-4'>
                         <div class='short-div'>
                             <label>Komponentenart:</label>
-                            <select data-placeholder='test placeholder' class='chosen-select' style='width: 150px;' onchange=\"filterTableBySelect(document.getElementById('table_left'), this.value);\">";
+                            <select disabled class='chosen-select selectKoArt' id='selectKoArt' style='width: 150px;' onchange=\"filterTableBySelect(document.getElementById('table_right$data_index'), this.value);\">";
                             
                             $strA="";
-                                            foreach (array_unique($komponenten_arten) as $komponent) {
+                                           $strA.= "<option value='all'>all</option>";
+                                            foreach ($components_art as $komponentart) {
 
-                                                $strA.= "<option value=\"".$komponent."\">".$komponent."</option>";
+                                                $strA.= "<option value=\"".$komponentart["ka_komponentenart"]."\">".$komponentart["ka_komponentenart"]."</option>";
                                             }
                             
                             $strB="</select>
@@ -75,66 +69,58 @@ $str1= "<div class='row'>
 					<div class='row'>
 						<div class='col-md-1'></div>
 						<div class='col-md-4'>	
-							<table class='table table-hover'>
+							<table id='table_left$data_index' class='table table-hover'>
 								<thead>
 								<tr>
 									<th>Name</th>
 									<th>Art</th>
+                                    <th style='width: 5px'></th>
 								</tr>
 								</thead>";
     $str2="";
     
-                                    if (!empty($komponenten)) {
-                                        
-                                        foreach ($komponenten as $komponent) {
-                                            $name = $komponent['name'];
-                                            $art = $komponent['art'];
-                                            
-                                            $str2 .= "<tbody>";
-                                            $str2 .= "<tr>";
-                                            $str2 .= "<td>".$name."</td>";
-                                            $str2 .= "<td>".$art."</td>";
-                                            $str2 .= "</tr>";
-                                            $str2 .= "</tbody>";
-                                        }
-                                        
-                                    }
+                
                                 
-$str3 = "
+	$str3 = "
                             </table>
 							</div>
 							<div class='col-md-1'>
 							</div>	
 							<div class='col-md-4'>";
-								
-                                        
-        
+								        
                                         
                                 $str5="
 								<div class='short-div'>
-								<table id='table_left' class='table table-hover'>
+								<table id='table_right$data_index' class='table table_right table-hover'>
 								<thead>
 								<tr>
 									<th>Name</th>
-									<th>Art</th>
+                                        <th>Art</th>
+                                        <th style='width: 5px'></th>
 								</tr>
 								</thead>";
                                 $str6="";
-                                    if (!empty($komponenten)) {
-                                        
-                                        foreach ($komponenten as $komponent) {
-                                            $name = $komponent['name'];
-                                            $art = $komponent['art'];
-                                            
-                                            $str6.= "<tbody>";
-                                            $str6.= "<tr>";
-                                            $str6.= "<td>".$name."</td>";
-                                            $str6.= "<td>".$art."</td>";
-                                            $str6.= "</tr>";
-                                            $str6.= "</tbody>";
+                                    if (!empty($asdasd)) {
+
+                                            foreach ($asdasd as $komponent) {
+                                                
+                                                $id = $komponent['k_id'];
+                                                $name = $komponent['k_name'];
+                                                $art = $komponent['ka_komponentenart'];
+
+                                                $str6.= "<tbody>";
+                                                $str6.= "<tr>";
+                                                $str6 .= "<input type='hidden' name='id' value='$id'>";
+                                                $str6.= "<td>".$name."</td>";
+                                                $str6.= "<td>".$art."</td>";
+                                                $str6 .= "<td> <div class='checkbox' >
+                                                        <input class='changeEditStatus' disabled type='checkbox' value=''>
+                                                      </div> </td>";
+                                                $str6.= "</tr>";
+                                                $str6.= "</tbody>";
+                                            }
+
                                         }
-                                        
-                                    }
 						$str7="	</table>
 								</div>
 							</div>
@@ -142,11 +128,11 @@ $str3 = "
 					<div class='row'>
 						<div class='col-md-4'></div>
 						
-							<input name='btnRem' type='button' class='btn btn-primary' value='entfernen'/>
+							<input name='btnRem' id='btnRem' onclick='removeKomponentenToCurrentList($data_index)' type='button' class='btn changeEditStatus btn-primary' disabled value='entfernen'/>
 						
                         <div class='col-md-4'></div>
 						
-							<input name='btnAdd' type='button' class='btn btn-primary' style='margin-left: 5px;' value='hinzufügen'/>
+							<input name='btnAdd' onclick=\"addKomponentenToCurrentList($data_index)\" id='btnAdd' type='button' class='btn changeEditStatus btn-primary' disabled style='margin-left: 5px;' value='hinzufügen'/>
 					</div>
                     <br/>
                     <div class='row'>
