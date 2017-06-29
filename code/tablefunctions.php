2e4be352ed4f9990f02f44c795b73735925fc238
Liddleriddle <sebastian.rittler@web.de>
2017-06-29 12:42:21 +0200

    bugfix
    verschachtelungs fehler

--- code/tablefunctions.php
+++ code/tablefunctions.php
@@ -67,7 +67,8 @@ class Rooms {
 		$result = sqlselect("raeume", $cols);
 		
 		$result = addtoarray($result,'komponenten','select k.*, a.* from komponentenarten a , komponenten k , komponente_in_raum i where i.r_id = ? and i.k_id = k.k_id and k.ka_id = a.ka_id','r_id');
-		foreach ($result as &$row){
+		foreach ($result as &$entry){
+			foreach ($entry as &$row){
 			$row = addtoarray($row,'lieferant','select * from lieferanten where l_id = ?','l_id');
 			$row = addtoarray($row,'komponentenattribute','select * from wird_beschrieben_durch b, komponentenattribute ka (left join komponente_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
 		}
