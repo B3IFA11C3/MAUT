d7cd7be5d8a9c5a496fa317b4c1c5e63dc2d41a9
Liddleriddle <sebastian.rittler@web.de>
2017-06-29 11:54:11 +0200

    rechtschreibung

--- code/tablefunctions.php
+++ code/tablefunctions.php
@@ -41,7 +41,7 @@ class Components {
     public static function list($cols = ["*"]) {
         $result = sqlselect("komponenten", $cols);
 		
-		$result = addtoarray($result,'liefeant','select * from lieferanten where l_id = ?','l_id');
+		$result = addtoarray($result,'lieferant','select * from lieferanten where l_id = ?','l_id');
 		$result = addtoarray($result,'komponentenattribute','select ka.* , h.* from wird_beschrieben_durch b, komponentenattribute ka (left join komponenten_hat_attribute h on h.kat_id = ka.kat_id and h.k_id = ?)where b.ka_id = ? and b.kat_id = ka.kat_id ',array('k_id','ka_id'));
 		$result = addtoarray($result,'raeume','select * from raeume r , komponente_in_raum i where r.r_id = i.r_id and i.k_id = ?','k_id');
         return $result;
