e94da78c2aa9c591940f8f8b5efea875fc646bce
Felix Schnizlein <felix@schnizle.in>
2017-06-29 13:57:43 +0200

    Add missing detail page for reporting lieferanten

--- modules/reporting/lieferanten/module.php
+++ modules/reporting/lieferanten/module.php
@@ -1,10 +1,13 @@
 <?php
 require_once("templates/details.php");
 
-function vendor() {
-  $result = mast_query_array("SELECT r_id, r_nr, r_bezeichnung, r_notiz
-                              FROM raeume
-                              WHERE r_geloescht != 1;");
+function vendors() {
+  $result = mast_query_array("SELECT l.l_id, l.l_firmennamen, l.l_strasse,
+                                     l.l_plz, l.l_ort, l.l_tel,  l.l_mobil,
+                                     l.l_fax, l.l_email, l.l_erstellt, 
+                                     l.l_geaendert
+                              FROM lieferanten l
+                              WHERE l_geloescht != 1;");
   return $result;
 }
 
---
+++ modules/reporting/lieferanten/templates/details.php
@@ -0,0 +1,6 @@
+<?php
+
+function render_vendor_details($vendor) {
+  $html = "";
+  return $html;
+}
